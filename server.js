2e88e721f4f33fc35dab714054ee29a4be885ebe
[Centro1993] <[jonas.leitner93@gmail.com]>
2017-04-01 03:50:06 +0200

    rescued the memory

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ io.on('connection', function(socket) {
 
             //send current xy values to tank
             console.log(xy);
-            tankBuffer.write(xy[0] + ';' + xy[1] + '*', 0);
+            tankBuffer.write(xy[0] + ';' + xy[1] + '*', 0, 'utf-8');
             tank.write(tankBuffer);
         }
     });
